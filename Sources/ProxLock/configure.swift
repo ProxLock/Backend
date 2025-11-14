0fc9aae62c3ad9c3b698fd138a0fd345ae2f6b65
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-14 00:51:39 -0800

    Update configure.swift

--- Sources/ProxLock/configure.swift
+++ Sources/ProxLock/configure.swift
@@ -5,6 +5,7 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    app.logger.warning("DATABASE_HOST: \(Environment.get("DATABASE_HOST"))")
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
