6eb12052ca20a785bccfcb0534151be42ee555e1
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2026-01-12 21:26:17 -0800

    fix cors

--- Sources/ProxLock/Middleware/CorsMiddleware.swift
+++ Sources/ProxLock/Middleware/CorsMiddleware.swift
@@ -9,7 +9,7 @@ import Foundation
 import Vapor
 
 private let corsConfiguration = CORSMiddleware.Configuration(
-    allowedOrigin: .any(["http://127.0.0.1:5173", "http://localhost:5173", "https://proxlock.dev", "https://app.proxlock.dev"]),
+    allowedOrigin: .any(["http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://localhost:5173",  "http://localhost:5174", "https://proxlock.dev", "https://app.proxlock.dev", "https://admin.proxlock.dev"]),
     allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
     allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin],
     allowCredentials: true
