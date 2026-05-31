0b93bf02aa9adbaab199dcb803f46f5a299075da
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2026-05-31 13:05:17 -0700

    move cors to be production and dev separately

--- Sources/ProxLock/Middleware/CorsMiddleware.swift
+++ Sources/ProxLock/Middleware/CorsMiddleware.swift
@@ -8,8 +8,16 @@
 import Foundation
 import Vapor
 
+private func getAllowedOrigins() -> [String] {
+    if let env = (try? Environment.detect()), env == .production {
+        return ["https://proxlock.dev", "https://app.proxlock.dev", "https://admin.proxlock.dev"]
+    } else {
+        return ["http://127.0.0.1:5173", "http://0.0.0.0:5173", "http://127.0.0.1:5174", "http://localhost:5173",  "http://localhost:5174", "https://proxlock.dev", "https://app.proxlock.dev", "https://admin.proxlock.dev"]
+    }
+}
+
 private let corsConfiguration = CORSMiddleware.Configuration(
-    allowedOrigin: .any(["http://127.0.0.1:5173", "http://127.0.0.1:5174", "http://localhost:5173",  "http://localhost:5174", "https://proxlock.dev", "https://app.proxlock.dev", "https://admin.proxlock.dev"]),
+    allowedOrigin: .any(getAllowedOrigins()),
     allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
     allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin, .code],
     allowCredentials: true,
